39cde039e747f18abca3bca8a9557bef9a30ae0d
SpeedyCloud0-0 <63207198+SpeedyCloud0-0@users.noreply.github.com>
2020-04-05 21:09:52 -0700

    Created a simple scene

--- Scrabble/src/GUI.java
+++ Scrabble/src/GUI.java
@@ -1,4 +1,31 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-public class GUI {
-
+public class GUI extends Application implements EventHandler<ActionEvent>{
+	 Button gameStart;
+	@Override
+	public void start(Stage gameWindow) throws Exception {
+		gameStart = new Button("Play");
+		
+		StackPane layout = new StackPane();
+		layout.getChildren().add(gameStart);
+		
+		Scene scene = new Scene(layout,800,800);
+		gameWindow.setScene(scene);
+		gameWindow.show();
+		
+	}
+	
+	
+	public void handle(ActionEvent event) {
+		if(event.getSource()==gameStart) {
+			
+		}
+	}
+	
 }
