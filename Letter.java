a140501e9f145b4d7761a6fa052a50e231f74569
Shodhan11 <59709509+Shodhan11@users.noreply.github.com>
2020-04-06 23:34:16 -0400

    Updated Letter.java, and Added JUnit test file for one of the methods in Letter.java

--- Letter.java
+++ Letter.java
@@ -5,7 +5,7 @@
  */
 public class Letter {
     private String letterName;
-    private int letterValue;
+    private int letterScore;
 
     /**
      * Constructor for Letter
@@ -13,103 +13,88 @@ public class Letter {
      */
     public Letter(String letterName) {
         this.letterName = letterName;
-        if (letterName.toUpperCase().equals("A"))  {
-            this.letterValue = 1;
-        }
-        if (letterName.toUpperCase().equals("B"))  {
-            this.letterValue = 3;
-        }
-        if (letterName.toUpperCase().equals("C"))  {
-            this.letterValue = 3;
-        }
-        if (letterName.toUpperCase().equals("D"))  {
-            this.letterValue = 2;
-        }
-        if (letterName.toUpperCase().equals("E"))  {
-            this.letterValue = 1;
-        }
-        if (letterName.toUpperCase().equals("F"))  {
-            this.letterValue = 4;
-        }
-        if (letterName.toUpperCase().equals("G"))  {
-            this.letterValue = 2;
-        }
-        if (letterName.toUpperCase().equals("H"))  {
-            this.letterValue = 4;
-        }
-        if (letterName.toUpperCase().equals("I"))  {
-            this.letterValue = 1;
-        }
-        if (letterName.toUpperCase().equals("J"))  {
-            this.letterValue = 8;
-        }
-        if (letterName.toUpperCase().equals("K"))  {
-            this.letterValue = 5;
-        }
-        if (letterName.toUpperCase().equals("L"))  {
-            this.letterValue = 1;
-        }
-        if (letterName.toUpperCase().equals("M"))  {
-            this.letterValue = 3;
-        }
-        if (letterName.toUpperCase().equals("N"))  {
-            this.letterValue = 1;
-        }
-        if (letterName.toUpperCase().equals("O"))  {
-            this.letterValue = 1;
-        }
-        if (letterName.toUpperCase().equals("P"))  {
-            this.letterValue = 3;
-        }
-        if (letterName.toUpperCase().equals("Q"))  {
-            this.letterValue = 10;
-        }
-        if (letterName.toUpperCase().equals("R"))  {
-            this.letterValue = 1;
-        }
-        if (letterName.toUpperCase().equals("S"))  {
-            this.letterValue = 1;
-        }
-        if (letterName.toUpperCase().equals("T"))  {
-            this.letterValue = 1;
-        }
-        if (letterName.toUpperCase().equals("U"))  {
-            this.letterValue = 1;
-        }
-        if (letterName.toUpperCase().equals("V"))  {
-            this.letterValue = 4;
-        }
-        if (letterName.toUpperCase().equals("W"))  {
-            this.letterValue = 4;
-        }
-        if (letterName.toUpperCase().equals("X"))  {
-            this.letterValue = 8;
-        }
-        if (letterName.toUpperCase().equals("Y"))  {
-            this.letterValue = 4;
-        }
-        if (letterName.toUpperCase().equals("Z"))  {
-            this.letterValue = 10;
-        }    
-        if (letterName.toUpperCase().equals("BLANK"))  {
-            this.letterValue = 0;
-        }                 
+        this.letterScore = convertNameToValue(letterName);                     
+    }
+    
+    /**
+     * Method convertNameToValue: This method gives the score associated with the Letter. So "A" gives a score of 1, "J" gives 8, and so on.
+     * @param s : This is the letter used for the letterScore
+     * @return : Returns the value used for letterScore
+     */
+    public int convertNameToValue(String s) {
+        int letterValue = 0;
+        if (letterName.toUpperCase().contentEquals("A"))  {
+            letterValue = 1;
+        } else if (letterName.toUpperCase().equals("B"))  {
+            letterValue = 3;
+        } else if (letterName.toUpperCase().equals("C"))  {
+            letterValue = 3;
+        } else if (letterName.toUpperCase().equals("D"))  {
+            letterValue = 2;
+        } else if (letterName.toUpperCase().equals("E"))  {
+            letterValue = 1;
+        } else if (letterName.toUpperCase().equals("F"))  {
+            letterValue = 4;
+        } else if (letterName.toUpperCase().equals("G"))  {
+            letterValue = 2;
+        } else if (letterName.toUpperCase().equals("H"))  {
+            letterValue = 4;
+        } else if (letterName.toUpperCase().equals("I"))  {
+            letterValue = 1;
+        } else if (letterName.toUpperCase().equals("J"))  {
+            letterValue = 8;
+        } else if (letterName.toUpperCase().equals("K"))  {
+            letterValue = 5;
+        } else if (letterName.toUpperCase().equals("L"))  {
+            letterValue = 1;
+        } else if (letterName.toUpperCase().equals("M"))  {
+            letterValue = 3;
+        } else if (letterName.toUpperCase().equals("N"))  {
+            letterValue = 1;
+        } else if (letterName.toUpperCase().equals("O"))  {
+            letterValue = 1;
+        } else if (letterName.toUpperCase().equals("P"))  {
+            letterValue = 3;
+        } else if (letterName.toUpperCase().equals("Q"))  {
+            letterValue = 10;
+        } else if (letterName.toUpperCase().equals("R"))  {
+            letterValue = 1;
+        } else if (letterName.toUpperCase().equals("S"))  {
+            letterValue = 1;
+        } else if (letterName.toUpperCase().equals("T"))  {
+            letterValue = 1;
+        } else if (letterName.toUpperCase().equals("U"))  {
+            letterValue = 1;
+        } else if (letterName.toUpperCase().equals("V"))  {
+            letterValue = 4;
+        } else if (letterName.toUpperCase().equals("W"))  {
+            letterValue = 4;
+        } else if (letterName.toUpperCase().equals("X"))  {
+            letterValue = 8;
+        } else if (letterName.toUpperCase().equals("Y"))  {
+            letterValue = 4;
+        } else if (letterName.toUpperCase().equals("Z"))  {
+            letterValue = 10;
+        } else if (letterName.toUpperCase().equals("BLANK"))  {
+            letterValue = 0;
+        }            
+        return letterValue;
     }
 
     /**
      * Getter method for letterName
      * @return
      */
-    public String getLetterName() {
+    public String getLetterName() {        
         return letterName;
     }
 
     /**
-     * Getter method for letterValue
+     * Getter method for letterScore
      * @return
      */
-    public int getLetterValue() {
-        return letterValue;
+    public int getLetterScore() {
+        return letterScore;
     }
     
     
---
+++ testLetter.java
@@ -0,0 +1,65 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class testLetter {
+
+    @Test
+    void testConvertNameToValue() {
+        //fail("Not yet implemented");
+        Letter myLetterA = new Letter("A");
+        assertEquals(myLetterA.getLetterScore(), 1);
+        Letter myLetterB = new Letter("B");
+        assertEquals(myLetterB.getLetterScore(), 3);
+        Letter myLetterC = new Letter("C");
+        assertEquals(myLetterC.getLetterScore(), 3);
+        Letter myLetterD = new Letter("D");
+        assertEquals(myLetterD.getLetterScore(), 2);
+        Letter myLetterE = new Letter("E");
+        assertEquals(myLetterE.getLetterScore(), 1);
+        Letter myLetterF = new Letter("F");
+        assertEquals(myLetterF.getLetterScore(), 4);
+        Letter myLetterG = new Letter("G");
+        assertEquals(myLetterG.getLetterScore(), 2);
+        Letter myLetterH = new Letter("H");
+        assertEquals(myLetterH.getLetterScore(), 4);
+        Letter myLetterI = new Letter("I");
+        assertEquals(myLetterI.getLetterScore(), 1);
+        Letter myLetterJ = new Letter("J");
+        assertEquals(myLetterJ.getLetterScore(), 8);
+        Letter myLetterK = new Letter("K");
+        assertEquals(myLetterK.getLetterScore(), 5);
+        Letter myLetterL = new Letter("L");
+        assertEquals(myLetterL.getLetterScore(), 1);
+        Letter myLetterM = new Letter("M");
+        assertEquals(myLetterM.getLetterScore(), 3);
+        Letter myLetterN = new Letter("N");
+        assertEquals(myLetterN.getLetterScore(), 1);
+        Letter myLetterO = new Letter("O");
+        assertEquals(myLetterO.getLetterScore(), 1);
+        Letter myLetterP = new Letter("P");
+        assertEquals(myLetterP.getLetterScore(), 3);
+        Letter myLetterQ = new Letter("Q");
+        assertEquals(myLetterQ.getLetterScore(), 10);
+        Letter myLetterR = new Letter("R");
+        assertEquals(myLetterR.getLetterScore(), 1);
+        Letter myLetterS = new Letter("S");
+        assertEquals(myLetterS.getLetterScore(), 1);
+        Letter myLetterT = new Letter("T");
+        assertEquals(myLetterT.getLetterScore(), 1);
+        Letter myLetterU = new Letter("U");
+        assertEquals(myLetterU.getLetterScore(), 1);
+        Letter myLetterV = new Letter("V");
+        assertEquals(myLetterV.getLetterScore(), 4);
+        Letter myLetterW = new Letter("W");
+        assertEquals(myLetterW.getLetterScore(), 4);
+        Letter myLetterX = new Letter("X");
+        assertEquals(myLetterX.getLetterScore(), 8);
+        Letter myLetterY = new Letter("Y");
+        assertEquals(myLetterY.getLetterScore(), 4);
+        Letter myLetterZ = new Letter("Z");
+        assertEquals(myLetterZ.getLetterScore(), 10);
+        Letter myLetterBlank = new Letter("BLANK");
+        assertEquals(myLetterBlank.getLetterScore(), 0);       
+    }
+}
