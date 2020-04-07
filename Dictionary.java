d5d1fe218e0075828da026f4c8760fb601c358f3
daisyliu-penn <63270248+daisyliu-penn@users.noreply.github.com>
2020-04-06 20:55:26 -0400

    Dictionary Class Milestone version by Daisy

--- Dictionary.java
+++ Dictionary.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.util.*;
 
 /**
@@ -11,6 +12,65 @@ public class Dictionary {
     public Dictionary() {
         dictionaryWordList = new ArrayList<String>();
         //Need to read the dictionary words from the dictionary.txt file
+
+
+        private ArrayList<String> dictionaryWordsAsList;
+/**
+ * Creates dictionary object
+ */
+	public Dictionary() {
+            this.dictionaryWordsAsList = new ArrayList<>();
+        }
+/**
+ * Copies to array
+ *
+ */
+        public void createAndCopyDictionaryToArray() {
+
+            // Create object for file
+            File dictionaryFile = new File("dictionary.txt");
+
+            // Read in dictionary from
+            try (BufferedReader br = new BufferedReader(
+                    new FileReader("dictionary.txt"))) {
+
+                String wordLine = br.readLine();
+
+                // As long as there's a next line..
+                while (wordLine != null) {
+
+                    // add word to word list
+                    this.dictionaryWordsAsList.add(wordLine);
+                    // System.out.println(wordLine);
+
+                    // Read in next line
+                    wordLine = br.readLine();
+
+                }
+            } catch (FileNotFoundException e) {
+
+                System.out.println("This file does not exist");
+                e.printStackTrace();
+
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+        public  ArrayList<String> getDictionaryWordsAsList() {
+            return this.dictionaryWordsAsList;
+        }
+
+        public void setDictionaryWordsAsList(ArrayList<String> dictionaryWordsAsList) {
+            this.dictionaryWordsAsList = dictionaryWordsAsList;
+        }
+
+        public static void main(String[] args) {
+            Dictionary dictionary = new Dictionary();
+            dictionary.createAndCopyDictionaryToArray();
+            System.out.println(dictionary.getDictionaryWordsAsList());
+        }
     }
 
 }
