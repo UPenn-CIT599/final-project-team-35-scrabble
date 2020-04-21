34102e8f3017ec8aa64b9529274ddb02e2c787a6
Shodhan11 <shodhan@seas.upenn.edu>
2020-04-20 20:56:45 -0400

    Updated FIles (includes DummyGUI, for reference for Actual GUI)
    These are updated files. I beleive two methods are remaining to complete the back-end of this Scrabble game. They pertain to the methods "checkError", and "playEvent" in the file "ScrabbleGame". Expect to finish this by tomorrow.

---
+++ Scrabble/src/DummyGUI.java
@@ -0,0 +1,168 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * This GUI class will interface with the front-end GUI of the Scrabble Game
+ * @author CIT-591 Team-35 Project
+ *
+ */
+public class DummyGUI {
+    //Open Welcome page after hitting "Run" for "ScrabbleRunner"
+    //Welcome Page Shows "Scrabble Game Label", "Click for Instructions button", "Game Options: with "Single Player" and "Multi-Player""
+    //Get Player names playing, then start game
+    //GameRunner selects first Player for first Turn. Player with Turn is communicated to GUI
+    //In game, Player gets filled Letter-Tray from "bagOfLetters"    
+    private int eventOptionButton;
+    private ArrayList<String> playerNames;
+    private HashMap<String, Integer> playerScores;
+    private int numPlayers;
+    private String name1;
+    private String name2;
+    private String currentPlayerName;
+    private boolean endGame;
+    private boolean playerEvent;
+    private boolean trayRefillEvent;
+    private boolean boardNewWordsEvent;
+    private ArrayList<String[]> newBoardPositions;
+    private ArrayList<String> newBoardLetters;   
+    private boolean wordsError;
+       
+    /**
+     * Constructor for GUI 
+     */
+    public DummyGUI() {
+        //eventOptionButton = 1;  
+        playerNames = new ArrayList<>();
+        newBoardPositions = new ArrayList<String[]>();
+        newBoardLetters = new ArrayList<String>(); 
+        wordsError = false;
+        endGame = false;
+    }
+    
+    public int getEventOptionButton() {
+        return eventOptionButton;
+    }
+    
+    public ArrayList<String> getPlayerNames() {
+        return playerNames;
+    }    
+    
+    public int getNumPlayers() {
+        return numPlayers;
+    }       
+    
+    public void welcomePage() {
+        System.out.println("Welcome to Scrabble Game");
+        System.out.println("Click Here for Instructions to the Game");
+        System.out.println("Select the Game option to play");
+        System.out.println("Click '1' for Single Player Game option, and build your word vocabulary!");
+        System.out.println("Click '2' for Two Player Game option, and show who has a better word vocabulary!");   
+        eventOptionButton = 2; //1;
+        //playerEvent = true;
+        
+        if (eventOptionButton == 1) {
+            //User selects "Single-Player Game
+            //instantiate ScrabbleGame with SinglePlayer option selected 
+            System.out.println("Single-Player ScrabbleGame Selected");
+            name1 = "Player-Name : John";
+            playerNames.add(name1);
+            numPlayers = this.playerNames.size();                       
+        }
+        else if (eventOptionButton == 2) {
+            //User selects "Multi-Player Game
+            //instantiate ScrabbleGame with MultiPlayer option selected    
+            System.out.println("Multi-Player ScrabbleGame Selected");
+            name1 = "Player-1 : John";   
+            System.out.println(name1);
+            playerNames.add(name1);
+            name2 = "Player-2 : Jill";        
+            System.out.println(name2);            
+            playerNames.add(name2); 
+            numPlayers = playerNames.size();
+        } 
+        
+        for (int i = 0; i < playerNames.size(); i++) {
+            String newName = playerNames.get(i);                  
+            playerScores.put(newName, 0);   //Creating new entry and score for Player with name via "playerName" 
+        }            
+        currentPlayerName = playerNames.get(0); 
+    }   
+    
+    public void displayScrabbleBoard(ArrayList<Cell[]> displayBoard) {
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 15; j++) {
+                System.out.print("" + displayBoard.get(i)[j].getCellValues() + "  ");                
+            }
+            System.out.println();
+        }         
+    }
+    
+    public void displayCurrentTray(ArrayList<Letter> displayTrayLetters) {
+        for (int i = 0; i < displayTrayLetters.size(); i++) {
+                System.out.print("" + displayTrayLetters.get(i).getLetterName() + "  ");                            
+        }    
+        System.out.println();        
+    }    
+ 
+    public String getCurrentPlayer() {
+        return currentPlayerName;
+    }
+    
+    public void setCurrentPlayer(String name) {
+        currentPlayerName = name;
+    }
+    
+    public ArrayList<String[]> getNewBoardPositions() {
+        return newBoardPositions;
+    }
+    
+    public void setNewBoardPositions() { //(ArrayList<String[]> resetNewBoardPositions) {
+        newBoardPositions.clear();// = resetNewBoardPositions;
+    }   
+    
+    public ArrayList<String> getNewBoardLetters() {
+        return newBoardLetters;
+    }
+    
+    public void setNewBoardLetters() {
+        newBoardLetters.clear();;
+    }  
+    
+    public void setPlayerScores(HashMap<String, Integer> updatedScores) {
+        playerScores.clear();
+        playerScores = updatedScores;
+    }      
+    
+    public boolean getPlayerEvent() {
+        return playerEvent;
+    }      
+    
+    public void setPlayerEvent(boolean resetPlayerEvent) {
+        playerEvent = resetPlayerEvent;
+    }      
+
+    public boolean getTrayRefillEvent() {
+        return trayRefillEvent;
+    }     
+    
+    public void setTrayRefillEvent(boolean resetRefillEvent) {
+        trayRefillEvent = resetRefillEvent;
+    }     
+    
+    public boolean getBoardNewWordsEvent() {
+        return boardNewWordsEvent;
+    }   
+    
+    public void setBoardNewWordsEvent(boolean resetBoardNewWordsEvent) {
+        boardNewWordsEvent = resetBoardNewWordsEvent;
+    }          
+    
+    public void setWordsError(boolean wordsSelectionError) {
+        wordsError = wordsSelectionError;
+    }  
+    
+    public boolean getEndGame() {
+        return endGame;
+    }    
+    
+}
--- Scrabble/src/ScrabbleGame.java
+++ Scrabble/src/ScrabbleGame.java
@@ -7,108 +7,205 @@ import java.util.*;
  *
  */
 public class ScrabbleGame {
-        private Random rand;
-        private ArrayList<Player> players;        
-        private BagOfLetters bagOfLetters;
-        private ScoreKeeper scoreKeeper;
-        private Dictionary dictionary;
-        private Letter currentLetter;
-        private String[] words;        
-        private Board board;
-        private boolean playing;
-        private boolean singlePlayerGame;
-        private boolean endGame;
-        //private GUI gui;
-        private int CurrentPlayerWordScore;
-        private ArrayList<String> CurrentPlayerWords;   
-        private int CurrentPlayerTotalScore;   
-        private HashMap<String, Integer> allPlayerScores;          
-        
-        /**
-         * ScrabbleGame Constructor
-         * @param option
-         */
-        public ScrabbleGame(int option, ArrayList<Player> allPlayers) {
-            rand = new Random();
-            dictionary = new Dictionary();
-            scoreKeeper = new ScoreKeeper();
-            //gui = new GUI(); //GUI is instantiating the game instead of the other way. GUI is acting like ScrabbleRunner!
-            singlePlayerGame = false;
-            playing = true;
-            players = new ArrayList<Player>();
-            players.addAll(allPlayers); //NOTE: player list added/instantiated in the GUI class, via inputs from the Players
-            
-            if (option == 1) {
-               singlePlayerGame = true;
-            } else {
-                singlePlayerGame = false;                
+    private boolean playing;    
+    private Random rand;
+
+    //Variables for Dictionary reference, for word check 
+    private Dictionary dictionary;
+    private ArrayList<String> dictionaryWords; 
+
+    //Variables for info on all Players: names, wordSelections      
+    private ArrayList<Player> players;  
+    private boolean singlePlayerGame; 
+    private Player currentPlayer;  
+    private int playerIndex;
+
+    //Variables for Board status        
+    private Board board;
+    private ArrayList<Cell[]> display;
+
+    private boolean refillLetterTray;
+    private BagOfLetters bagOfLetters;
+    public ScoreKeeper scoreKeeper;
+
+    private Letter currentLetter;
+    private String[] words;        
+
+    private boolean endGame;
+    //private GUI gui;
+    private int CurrentPlayerWordScore;
+    private ArrayList<String> CurrentPlayerWords;   
+    private int CurrentPlayerTotalScore;   
+    private HashMap<String, Integer> allPlayerScores;  
+    //private ArrayList<Integer> allPlayerScores;         
+
+
+    /**
+     * ScrabbleGame Constructor
+     * @param option
+     */
+    //public ScrabbleGame(int option, ArrayList<Player> allPlayers) {
+    public ScrabbleGame(ArrayList<String> allPlayerNames) {   
+        playing = true;               
+        rand = new Random(); 
+        createDictionary(); //Creating Dictionary for exhaustive word reference
+        initializePlayers(allPlayerNames); //Initializing Players playing this game       
+        board = new Board(); //Initializing Board, and sending status of board to GUI   
+        playerIndex = 0;
+        currentPlayer = players.get(playerIndex);         
+        bagOfLetters = new BagOfLetters();
+        //Initializing Score Keeper           
+        scoreKeeper = new ScoreKeeper();            
+    }
+
+    public void createDictionary() {
+        //Creating reference Dictionary for error-check with word choices. 
+        //NOTE: Remove or Ignore ln-1 and ln-2 when referencing this "CollinsScrabbleWords2019.txt" dictionary
+        dictionary = new Dictionary();
+        dictionary.createAndCopyDictionaryToArray();
+        dictionaryWords = dictionary.getDictionaryWordsAsList();
+        dictionaryWords.remove(0);
+        dictionaryWords.remove(1);             
+    }
+
+    public void initializePlayers(ArrayList<String> playerNames) {
+        singlePlayerGame = true; //Initial assumption. To be changed if more than one player is playing              
+        //Initializing all the players for this game.
+        //Initial draft allows 2 Players, but final version may allow upto 4 Players. Show error if more than 4 Players!!!
+        players = new ArrayList<Player>();            
+        for (int i=0; i < playerNames.size(); i++) {                    
+            Player player1 = new Player(playerNames.get(0));
+            players.add(player1);   
+            if (playerNames.size() > 1) {
+                singlePlayerGame = false;                          
+                Player player2 = new Player(playerNames.get(1));                    
+                players.add(player2);     
+                if (playerNames.size() > 2) {
+                    Player player3 = new Player(playerNames.get(2));                    
+                    players.add(player3);                     
+                    if (playerNames.size() > 3) {  
+                        Player player4 = new Player(playerNames.get(3));                    
+                        players.add(player4); 
+                    }
+                }                          
             }
+        }            
+    }
+
+    /**
+     * This method checks the word selection of the current Player, and returns an error if wrong, else updates the current Players tray, and updates the Board in the game of 'Scrabble'
+     */
+    public void playEvent(ArrayList<String[]> boardPositions, ArrayList<String> boardLetters) {
+        //String status; //dummy string. May or may not use it
+        ArrayList<Letter> trayOfLetters = player.getCurrentLetterTray();
+        int sizeOfCurrentTray = player.getCurrentLetterTray().size();
+        boolean refillFullTray = false;
+        
+        if (refillLetterTray) {
+            ArrayList<Letter> newLetters = new ArrayList<Letter>();
+            newLetters = bagOfLetters.shuffleAndPickLetters(7 - sizeOfCurrentTray);
+            currentPlayer.trayOfLetters(refillFullTray, newLetters);          
         }
         
-        /**
-         * This method plays the game of 'Scrabble'
-         */
-        public void play() {
-            String status; //dummy string. May or may not use it
-            while (playing) {
-                bagOfLetters = new BagOfLetters();
-                board = new Board();
-                while ((!bagOfLetters.isEmpty()) || !endGame) {
-                    status = "Playing";                   
-                    /*/TODO: Figure out how to communciate with the GUI via JavaFx
+        while (playing) {
+            //bagOfLetters = new BagOfLetters();
+            //board = new Board();
+            if ((!bagOfLetters.isEmpty()) || (!endGame)) {
+                if ()
+                
+                //status = "Playing";                   
+                /*/TODO: Figure out how to communicate with the GUI via JavaFx
                     playerTurn();
                     display();
                     playerTurn(); 
                     play, playerTurn, getScore, gameResult, playAgain, reStartPlay */
-                }
             }
-            //gui.setClose();
+            else {
+                playing = false;
+            }
         }
+        //gui.setClose();
+    }
+    
+    /**
+     * This method refills the LetterTray of the current player playing the game of 'Scrabble'
+     */
+    public void refillEvent() {
+        //String status; //dummy string. May or may not use it
+        //ArrayList<Letter> trayOfLetters = currentPlayer.getCurrentLetterTray();
+        int sizeOfCurrentTray = currentPlayer.getCurrentLetterTray().size();
+        boolean refillFullTray = false;
+        ArrayList<Letter> newLetters = new ArrayList<Letter>();
+        newLetters = bagOfLetters.shuffleAndPickLetters(7 - sizeOfCurrentTray);
+        currentPlayer.trayOfLetters(refillFullTray, newLetters);          
+    }    
 
-        /**
-         * This method displays in the GUI: the Board, score and the status of the Player with the current Turn.
-         */
-        private void display() {
-            CurrentPlayerWordScore = 0; //dummyWordScore
-            CurrentPlayerWords.add("dummyWord1");  //This could be an ArrayList<String>
-            CurrentPlayerTotalScore = 0; //dummyTotalScore
-            board.currentBoard();   
-            gameResult();
+    /**
+     * This method displays in the GUI: the Board, score and the status of the Player with the current Turn.
+     */
+    public ArrayList<Cell[]> getDisplay() {
+        //CurrentPlayerWordScore = 0; //dummyWordScore
+        //CurrentPlayerWords.add("dummyWord1");  //This could be an ArrayList<String>
+        //CurrentPlayerTotalScore = 0; //dummyTotalScore
+        display = new ArrayList<Cell[]>();        
+        display = board.getCurrentBoard();   
+        //gameResult();
+        return display;
+    } 
+
+    public void changePlayer() {
+        //boolean newPlayersTurn = changePlayer;
+        if (playerIndex == (players.size() - 1)) {
+            playerIndex = 0;
+        } else {
+            playerIndex += 1;
         }
-        
-        private void playerTurn() {
-            boolean complete = false;
-            while (!complete) {
-                String currentPlayer = "dummyPlayer";
-            }
+        currentPlayer = players.get(playerIndex);                  
+    }
+    
+    public boolean checkError() {
+        boolean newWordsError = false;
+        return newWordsError;
+    }
+    
+    public Player getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    /**
+     * This method sends out the scores of each Player from the game to the GUI.
+     * @return
+     */
+    private void gameResult() {
+        allPlayerScores = scoreKeeper.getPlayerScores();
+    }
+
+    /**
+     * This method determines whether the user will play again.
+     */
+    private void playAgain() {
+        String inputFromGUI = "Play Again"; //dummy String. Needs input from GUI
+        if (inputFromGUI.toLowerCase().equals("play again")) {
+            playing = true;
         }
-        
-        /**
-         * This method sends out the scores of each Player from the game to the GUI.
-         * @return
-         */
-        private void gameResult() {
-            allPlayerScores = scoreKeeper.getPlayerScores();
+    }
+
+    /**
+     * This method determines whether the user will restart play.
+     * Scrabble is a long game, and is the Players want to restart play, this method can be called to restart the Scrabble Game
+     */
+    private void restartPlay() {
+        String inputFromGUI = "Restart Play"; //dummy String. Needs input from GUI
+        if (inputFromGUI.toLowerCase().equals("restart play")) {
+            playing = true;
         }
-        
-        /**
-         * This method determines whether the user will play again.
-         */
-        private void playAgain() {
-            String inputFromGUI = "Play Again"; //dummy String. Needs input from GUI
-            if (inputFromGUI.toLowerCase().equals("play again")) {
-                playing = true;
-            }
-        }
-        
-        /**
-         * This method determines whether the user will restart play.
-         * Scrabble is a long game, and is the Players want to restart play, this method can be called to restart the Scrabble Game
-         */
-        private void restartPlay() {
-            String inputFromGUI = "Restart Play"; //dummy String. Needs input from GUI
-            if (inputFromGUI.toLowerCase().equals("restart play")) {
-                playing = true;
-            }
-        }        
+    }    
+    
+    public boolean getBagOfLettersIsEmpty() {
+        return bagOfLetters.isEmpty();
+    }
+    
+    public void setEndGame(boolean gameStatus) {
+        endGame = gameStatus;
+    }    
 }
